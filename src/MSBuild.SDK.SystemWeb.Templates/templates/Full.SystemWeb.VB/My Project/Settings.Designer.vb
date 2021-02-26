6c68d8769adc88bf792d26150e321c6ae9794182
Cynthia Z E MacLeod (C3D) <Cynthia.MacLeod@cloud3d.ltd>
2021-02-26 19:01:48 +0000

    Fix bad Settings.Designer in Full.SystemWeb.VB template

--- src/MSBuild.SDK.SystemWeb.Templates/templates/Full.SystemWeb.VB/My Project/Settings.Designer.vb
+++ src/MSBuild.SDK.SystemWeb.Templates/templates/Full.SystemWeb.VB/My Project/Settings.Designer.vb
@@ -63,10 +63,10 @@ Namespace My
      Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute()> _
     Friend Module MySettingsProperty
 
-        <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")> _
-        Friend ReadOnly Property Settings() As Global.ExampleFullWebApplication.My.MySettings
+        <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>
+        Friend ReadOnly Property Settings() As My.MySettings
             Get
-                Return Global.ExampleFullWebApplication.My.MySettings.Default
+                Return My.MySettings.Default
             End Get
         End Property
     End Module
