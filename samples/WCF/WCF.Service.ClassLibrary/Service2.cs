13a2e5febc55ffaab27ee55c829191c1a5ae4a09
Cynthia Z E MacLeod (C3D) <Cynthia.MacLeod@cloud3d.ltd>
2022-03-13 03:51:40 +0000

    Add WCF sample [skip ci]

---
+++ samples/WCF/WCF.Service.ClassLibrary/Service2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+
+namespace WCF.Service.ClassLibrary;
+
+[ServiceContract]
+public interface ICalculator2
+{
+	[OperationContract]
+	[WebGet]
+	double Add(double n1, double n2);
+	[OperationContract]
+	[WebGet]
+	double Subtract(double n1, double n2);
+	[OperationContract]
+	[WebGet]
+	double Multiply(double n1, double n2);
+	[OperationContract]
+	[WebGet]
+	double Divide(double n1, double n2);
+}
+
+[AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+public class CalculatorService2 : ICalculator2
+{
+	public double Add(double n1, double n2)
+	{
+		return n1 + n2;
+	}
+	public double Subtract(double n1, double n2)
+	{
+		return n1 - n2;
+	}
+	public double Multiply(double n1, double n2)
+	{
+		return n1 * n2;
+	}
+	public double Divide(double n1, double n2)
+	{
+		return n1 / n2;
+	}
+}
---
+++ samples/WCF/WCF.Service.WebApplication/global.asax
@@ -0,0 +1,16 @@
+﻿<%@ Application Language="C#" %>  
+<%@ Import Namespace="System.Web.Routing" %>  
+<%@ Import Namespace="System.ServiceModel.Activation" %>  
+<%@ Import Namespace="System.ServiceModel.Web " %>  
+  
+<script RunAt="server">  
+    void Application_Start(object sender, EventArgs e)  
+    {  
+        RegisterRoutes(RouteTable.Routes);  
+    }  
+  
+    private void RegisterRoutes(RouteCollection routes)  
+    {  
+        routes.Add(new ServiceRoute("service2", new WebServiceHostFactory(), typeof(WCF.Service.ClassLibrary.CalculatorService2)));
+   }  
+</script>
