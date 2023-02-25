b2e133c3e01f85d61afce1ee2d57fda01306f328
leusbj <103057560+leusbj@users.noreply.github.com>
2023-02-25 16:49:08 -0600

    Unit Test to cover Sdks ability to set Default Version metadata for Default PackageReferences
    Cherry Pick Unit test from other branch and alter to exercise the Default PackageReference items and Default Version Metadata when Some Flavor of Central Management of Package is enabled on Project Repo
    This Commit basically adds 3 simple cases
    1. No Central Management and SDK adds both PackageReference as well as Version Metadata
    2. CentralPackageVersionSdk is presentt but doesn't contain Version metadata for the default packages, and SDK must set the version metadtada
    3. CentralPackageManagement is present but doesn't contain Version metadata for the default packages, and SDK must set the version metadata

--- src/MSBuild.SDK.SystemWeb.UnitTests/DefaultPackageTests.cs
+++ src/MSBuild.SDK.SystemWeb.UnitTests/DefaultPackageTests.cs
@@ -24,7 +24,7 @@ namespace MSBuild.SDK.SystemWeb.UnitTests
         [Theory]
         [InlineData(".csproj")]
         //[InlineData(false, ".vbproj")]
-        public void DefaultPacakges_WhenNoCentralManagement_ArePresent(string extension)
+        public void DefaultPacakges_WhenNoCentralManagement_VersionMetadataIsPresent(string extension)
         {
             ProjectCreator.Create()
                 .Save(Path.Combine(TestRootPath, "Directory.Build.props"));
@@ -45,6 +45,7 @@ namespace MSBuild.SDK.SystemWeb.UnitTests
                 .Save(Path.Combine(TestRootPath, "BeforeMS_NET_SDk_targets.targets"));
 
             ProjectCreator.Create()
+                .Import(Path.Combine(ThisAssemblyDirectory, @"TestableSdkComponents\Sdk\MSBuild.SDK.SystemWeb.DefaultPackageControlledVersions.targets"))
                 .Save(Path.Combine(TestRootPath, "AfterMS_NET_SDk_targets.targets"));
 
             var projVal = ProjectCreator.Templates
@@ -63,30 +64,36 @@ namespace MSBuild.SDK.SystemWeb.UnitTests
         [Theory]
         [InlineData(".csproj")]
         //[InlineData(false, ".vbproj")]
-        public void DefaultPacakges_WhenCentralManagement_ArePresent(string extension)
+        public void DefaultPacakges_WhenCentralPackageVersionSdkDoesNOTHaveVersion_VersionMetadataDefaultsAreProvidedBySdk(string extension)
         {
-            ProjectCreator.Create()
-                .Save(Path.Combine(TestRootPath, "Directory.Build.props"));
-
             ProjectCreator.Create()
                 .Import(Path.Combine(ThisAssemblyDirectory, @"TestableSdkComponents\Sdk\MSBuild.SDK.SystemWeb.DefaultPackages.props"))
                 .Save(Path.Combine(TestRootPath, "BeforeMS_NET_SDk_props.props"));
+
+            ProjectCreator.Create()
+                .Save(Path.Combine(TestRootPath, "Directory.Build.props"));
 
             ProjectCreator.Create()
                 .Save(Path.Combine(TestRootPath, "AfterMS_NET_SDk_props.props"));
 
 
             ProjectCreator.Create()
-                .ImportSdk(name: "Microsoft.Build.CentralPackageVersions", version:"2.1.3",project:"Sdk.props")
+                .Import(Path.Combine(ThisAssemblyDirectory, @"TestableSdkComponents\Sdk\MSBuild.SDK.SystemWeb.DefaultPackages.targets"))
+                .ItemGroup().ItemInclude("PackageReferencesAddedBySdkAsAddedWithoutVersionMetadata", "@(PackageReference)")
+                .Save(Path.Combine(TestRootPath, "BeforeMS_NET_SDk_targets.targets"));
+
+            ProjectCreator.Create()
+                .Save(Path.Combine(TestRootPath, "Packages.props"));
+
+            ProjectCreator.Create()
+                .ImportSdk(name: "Microsoft.Build.CentralPackageVersions", version: "2.1.3", project: "Sdk.props")
                 .ImportSdk(name: "Microsoft.Build.CentralPackageVersions", version: "2.1.3", project: "Sdk.targets")
                 .Save(Path.Combine(TestRootPath, "Directory.Build.targets"));
 
             ProjectCreator.Create()
-                .Import(Path.Combine(ThisAssemblyDirectory, @"TestableSdkComponents\Sdk\MSBuild.SDK.SystemWeb.DefaultPackages.targets"))
-                .ItemGroup().ItemInclude("PackageReferenceBefore", "@(PackageReference)")
-                .Save(Path.Combine(TestRootPath, "BeforeMS_NET_SDk_targets.targets"));
-
-            ProjectCreator.Create()
+                .Import(Path.Combine(ThisAssemblyDirectory, @"TestableSdkComponents\Sdk\MSBuild.SDK.SystemWeb.DefaultPackageControlledVersions.targets"))
+                .ItemGroup().ItemInclude("PackageReferencesAfterSdkUpdatedVersionMetadata", "@(PackageReference)")
+                .ItemGroup().ItemInclude("SystemWebSdkProvidedPackageVersionDefault_Audit", "@(SystemWebSdkProvidedPackageVersionDefault)")
                 .Save(Path.Combine(TestRootPath, "AfterMS_NET_SDk_targets.targets"));
 
             var projVal = ProjectCreator.Templates
@@ -95,15 +102,97 @@ namespace MSBuild.SDK.SystemWeb.UnitTests
                 targetFramework: "net47",
                 sdk: Path.Combine(ThisAssemblyDirectory, @"TestHarnessInjectionSdk")
                 );
-            projVal.TryGetItems("PackageReference", out IReadOnlyCollection<ProjectItem> PackRef);
-            PackRef.ShouldNotBe(null);
-
-            projVal.TryGetItems("PackageReferenceBefore", out IReadOnlyCollection<ProjectItem> PackRefAutdit);
-            PackRefAutdit.Where(i => i.GetMetadataValue("Version") == "").Count().ShouldBe(2);
 
 
 
+            projVal.TryGetItems("PackageReferencesAddedBySdkAsAddedWithoutVersionMetadata", out IReadOnlyCollection<ProjectItem> PackageReferencesAddedBySdkAsAddedWithoutVersionMetadata);
+            PackageReferencesAddedBySdkAsAddedWithoutVersionMetadata.ShouldNotBe(null);
+            PackageReferencesAddedBySdkAsAddedWithoutVersionMetadata.Count().ShouldBe(2);
+            PackageReferencesAddedBySdkAsAddedWithoutVersionMetadata.Where(i => i.GetMetadataValue("Version") == "").Count().ShouldBe(2);
 
+            projVal.TryGetItems("PackageReferencesAfterSdkUpdatedVersionMetadata", out IReadOnlyCollection<ProjectItem> PackageReferencesAfterSdkUpdatedVersionMetadata);
+            PackageReferencesAfterSdkUpdatedVersionMetadata.ShouldNotBe(null);
+            PackageReferencesAfterSdkUpdatedVersionMetadata.Count().ShouldBe(2);
+            PackageReferencesAfterSdkUpdatedVersionMetadata.Where(i => i.GetMetadataValue("Version") != "").Count().ShouldBe(2);
+
+            projVal.TryGetItems("SystemWebSdkProvidedPackageVersionDefault_Audit", out IReadOnlyCollection<ProjectItem> SystemWebSdkProvidedPackageVersionDefault_Audit);
+            SystemWebSdkProvidedPackageVersionDefault_Audit.ShouldNotBe(null);
+            SystemWebSdkProvidedPackageVersionDefault_Audit.Count().ShouldBe(2);
+
+            projVal.TryGetPropertyValue("ExcludeASPNetCompilers", out string Property_ExcludeASPNetCompilers);
+            Property_ExcludeASPNetCompilers.ShouldNotBe("False");
+            //SystemWebSdkProvidedPackageVersionDefault
+            projVal.TryGetPropertyValue("UsingMicrosoftCentralPackageVersionsSdk", out string Property_UsingMicrosoftCentralPackageVersionsSdk);
+            projVal.TryGetPropertyValue("EnableCentralPackageVersions", out string Property_EnableCentralPackageVersions);
+            projVal.TryGetPropertyValue("ManagePackageVersionsCentrally", out string Property_ManagePackageVersionsCentrally);
+
+
+        }
+
+
+        [Theory]
+        [InlineData(".csproj")]
+        //[InlineData(false, ".vbproj")]
+        public void DefaultPacakges_WhenCentralPackageManagementSdkDoesNOTHaveVersion_VersionMetadataDefaultsAreProvidedBySdk(string extension)
+        {
+            ProjectCreator.Create()
+                .Import(Path.Combine(ThisAssemblyDirectory, @"TestableSdkComponents\Sdk\MSBuild.SDK.SystemWeb.DefaultPackages.props"))
+                .Save(Path.Combine(TestRootPath, "BeforeMS_NET_SDk_props.props"));
+
+            ProjectCreator.Create()
+                .Save(Path.Combine(TestRootPath, "Directory.Build.props"));
+
+            ProjectCreator.Create()
+                .Save(Path.Combine(TestRootPath, "AfterMS_NET_SDk_props.props"));
+
+
+            ProjectCreator.Create()
+                .Import(Path.Combine(ThisAssemblyDirectory, @"TestableSdkComponents\Sdk\MSBuild.SDK.SystemWeb.DefaultPackages.targets"))
+                .ItemGroup().ItemInclude("PackageReferencesAddedBySdkAsAddedWithoutVersionMetadata", "@(PackageReference)")
+                .Save(Path.Combine(TestRootPath, "BeforeMS_NET_SDk_targets.targets"));
+
+            ProjectCreator.Create()
+                .PropertyGroup().Property("ManagePackageVersionsCentrally","true")
+                .Save(Path.Combine(TestRootPath, "Directory.Packages.props"));
+
+            ProjectCreator.Create()
+                .Save(Path.Combine(TestRootPath, "Directory.Build.targets"));
+
+            ProjectCreator.Create()
+                .Import(Path.Combine(ThisAssemblyDirectory, @"TestableSdkComponents\Sdk\MSBuild.SDK.SystemWeb.DefaultPackageControlledVersions.targets"))
+                .ItemGroup().ItemInclude("PackageVersionAfterSdkUpdatedVersionMetadata", "@(PackageVersion)")
+                .ItemGroup().ItemInclude("SystemWebSdkProvidedPackageVersionDefault_Audit", "@(SystemWebSdkProvidedPackageVersionDefault)")
+                .Save(Path.Combine(TestRootPath, "AfterMS_NET_SDk_targets.targets"));
+
+            var projVal = ProjectCreator.Templates
+                .SdkCsproj(
+                    path: Path.Combine(TestRootPath, $"test{extension}"),
+                targetFramework: "net47",
+                sdk: Path.Combine(ThisAssemblyDirectory, @"TestHarnessInjectionSdk")
+                );
+
+
+
+            projVal.TryGetItems("PackageReferencesAddedBySdkAsAddedWithoutVersionMetadata", out IReadOnlyCollection<ProjectItem> PackageReferencesAddedBySdkAsAddedWithoutVersionMetadata);
+            PackageReferencesAddedBySdkAsAddedWithoutVersionMetadata.ShouldNotBe(null);
+            PackageReferencesAddedBySdkAsAddedWithoutVersionMetadata.Count().ShouldBe(2);
+            PackageReferencesAddedBySdkAsAddedWithoutVersionMetadata.Where(i => i.GetMetadataValue("Version") == "").Count().ShouldBe(2);
+
+            projVal.TryGetItems("PackageVersionAfterSdkUpdatedVersionMetadata", out IReadOnlyCollection<ProjectItem> PackageVersionAfterSdkUpdatedVersionMetadata);
+            PackageVersionAfterSdkUpdatedVersionMetadata.ShouldNotBe(null);
+            PackageVersionAfterSdkUpdatedVersionMetadata.Count().ShouldBe(2);
+            PackageVersionAfterSdkUpdatedVersionMetadata.Where(i => i.GetMetadataValue("Version") != "").Count().ShouldBe(2);
+
+            projVal.TryGetItems("SystemWebSdkProvidedPackageVersionDefault_Audit", out IReadOnlyCollection<ProjectItem> SystemWebSdkProvidedPackageVersionDefault_Audit);
+            SystemWebSdkProvidedPackageVersionDefault_Audit.ShouldNotBe(null);
+            SystemWebSdkProvidedPackageVersionDefault_Audit.Count().ShouldBe(2);
+
+            projVal.TryGetPropertyValue("ExcludeASPNetCompilers", out string Property_ExcludeASPNetCompilers);
+            Property_ExcludeASPNetCompilers.ShouldNotBe("False");
+            //SystemWebSdkProvidedPackageVersionDefault
+            projVal.TryGetPropertyValue("UsingMicrosoftCentralPackageVersionsSdk", out string Property_UsingMicrosoftCentralPackageVersionsSdk);
+            projVal.TryGetPropertyValue("EnableCentralPackageVersions", out string Property_EnableCentralPackageVersions);
+            projVal.TryGetPropertyValue("ManagePackageVersionsCentrally", out string Property_ManagePackageVersionsCentrally);
         }
     }
 }
